23d2cbaa320a03e163adc5c86069f2b5c1d96b13
nightguarder <nigthguarder@gmail.com>
2023-06-27 20:18:00 +0200

    sanity fully integrated.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,12 +1,9 @@
-import { loadEnvConfig } from '@next/env'
+/**
+* This configuration file lets you run `$ sanity [command]` in this folder
+* Go to https://www.sanity.io/docs/cli to learn more.
+**/
 import { defineCliConfig } from 'sanity/cli'
 
-const dev = process.env.NODE_ENV !== 'production'
-loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
-
-// @TODO report top-level await bug
-// Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
-// const { projectId, dataset } = await import('lib/sanity.api')
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
